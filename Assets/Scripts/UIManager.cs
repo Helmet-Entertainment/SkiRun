c0bd274a6071e32018605c3ec034bd381159369d
Batuhan Çelenk <batuhanelenk@gmail.com>
2021-11-27 18:10:53 +0300

    Progress Bar Added.

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -3,20 +3,55 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
     public TMP_Text scoreText;
     public static UIManager instance;
     public int theScore;
-   
+
+    [SerializeField] private Image uiProgressFill;
+    [SerializeField] private Transform playerTransform;
+    [SerializeField] private Transform endLineTransform;
+    
+    private Vector3 endLinePosition;
+    private float fullDistance;
+
     private void Awake()
     {
         instance = this;
     }
 
+    private void Start()
+    {
+        endLinePosition = endLineTransform.position;
+        fullDistance = GetDistance();
+    }
+
+    private void Update()
+    {
+        if (playerTransform.position.z <= endLinePosition.z)
+        {
+            float newDistance = GetDistance();
+            float progressValue = Mathf.InverseLerp(fullDistance, 0f, newDistance);
+            UpdateProgressFill(progressValue);   
+        }
+    }
+
+    private float GetDistance()
+    {
+        return Vector3.Distance(playerTransform.position, endLinePosition);
+    }
+
+    private void UpdateProgressFill(float value)
+    {
+        uiProgressFill.fillAmount = value;
+    }
+    
     public void UpdateScore()
     {
         scoreText.text = "Fans: " + theScore;
     }
+    
 }
