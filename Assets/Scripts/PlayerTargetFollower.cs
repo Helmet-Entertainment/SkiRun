5e0f4e83ebc7a4d6cabfb3bff693822dd4d5fb58
tolgasylll <75844879+tolgasylll@users.noreply.github.com>
2021-11-29 23:59:46 +0300

    Add flag model, Change Player Movement

--- Assets/Scripts/PlayerTargetFollower.cs
+++ Assets/Scripts/PlayerTargetFollower.cs
@@ -8,7 +8,9 @@ public class PlayerTargetFollower : MonoBehaviour
 {
     private NavMeshAgent agent;
     [SerializeField] private Transform target;
-
+    [SerializeField] private LayerMask groundLayerMask;
+    [SerializeField] private Transform playerGraphic;
+    private float velocity = 20f;
     private void Start()
     {
         agent = GetComponent<NavMeshAgent>();
@@ -17,5 +19,19 @@ public class PlayerTargetFollower : MonoBehaviour
     private void Update()
     {
         agent.destination = target.position;
+        RotatePlayerToFloor();
+    }
+
+    private void RotatePlayerToFloor()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position+Vector3.up, transform.TransformDirection(Vector3.down), out hit, 10f, groundLayerMask))
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.down) * hit.distance, Color.yellow);
+            playerGraphic.up = hit.normal;
+            var rotation = playerGraphic.rotation;
+            rotation.y = this.transform.rotation.y;
+            playerGraphic.rotation = rotation;
+        }
     }
 }
